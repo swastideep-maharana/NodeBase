f66063e9c709488bd7074e6d623b4237a29d9dbf
swastideep-maharana <swastideep67maharana@gmail.com>
2025-11-17 16:13:02 +0530

    components setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-const Page = ()=> {
-  return ( 
-    <div className="text-7xl text-red-500 font-extrabold justify-center items-center flex h-screen">
-      Hello, World!
+import { cn } from "@/lib/utils";
+
+const Page = ()=>{
+  const someting = true;
+
+  return(
+    <div className={cn("text-7xl font-medium", someting && "text-red-500 justify-center flex items-center h-screen")}>
+      Hello World!
     </div>
-  );
-};
+  )
+}
 
 export default Page;
